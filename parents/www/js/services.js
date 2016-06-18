5efd48b0ac5e0ff5dbaeab4734370149706b7cc1
cryt1c <david@peherstorfer.info>
2016-06-18 19:41:16 +0200

    fixed the statistic in the parent app: -fixed legend with multiple categories -fixed n/w display

--- parents/www/js/services.js
+++ parents/www/js/services.js
@@ -51,7 +51,7 @@ angular.module('starter.services', [])
     };
 
     this.createTransaction = function (recipient, amount, category, is_need) {
-      return {recipient: recipient, amount: amount, category: category, isNeed: is_need, writtenToServer: false};
+      return {recipient: recipient, amount: amount, category: category, is_need: is_need, writtenToServer: false};
     };
 
     this.addTransaction = function (transaction) {
@@ -62,13 +62,13 @@ angular.module('starter.services', [])
 
     this.loadTransactionsJSON = function (existingTransactions) {
       //_transactions = existingTransactions;
-      _transactions = _transactions.filter(function(value) {
+      _transactions = _transactions.filter(function (value) {
         return (value.type == 1);
       });
       for (var i = 0; i < existingTransactions.length; i++) {
         var thisT = existingTransactions[i];
         var trans = this.createAndAddTransaction(thisT.recipient, thisT.amount, thisT.category);
-        trans.isNeed = thisT.is_need;
+        trans.is_need = thisT.is_need;
         trans.date = new Date(thisT.timestamp);
         trans.writtenToServer = true; // flag this as already being written to the server
       }
@@ -159,29 +159,219 @@ angular.module('starter.services', [])
       icon_ios: 'ion-heart'
     }];
 
-    /*transactions= [{
-      id: 1,
-      amount: 10,
-      recipient: "Spar",
-      category: 1,
-      date: "2016-05-19T00:00:00.000Z",
-      is_need: true,
-      child_id: 1,
-      timestamp: "2016-05-19T12:31:54.624Z"
-    },
-      {
-        id: 2,
-        amount: 2.5,
-        recipient: "Billa",
-        category: 3,
-        date: "2016-05-04T00:00:00.000Z",
-        is_need: false,
-        child_id: 1,
-        timestamp: "2016-05-04T12:31:54.624Z"
-      }];*/
 
     transactions = transactionsService.transactions();
 
+/*    transactions = [{
+      "id": 3,
+      "amount": 2.49,
+      "recipient": "Libro",
+      "category": 3,
+      "date": "2016-05-19T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-05-19T13:13:54.624Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 8,
+      "amount": 3.14,
+      "recipient": "Freudensprung",
+      "category": 6,
+      "date": "2016-05-31T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-05-31T16:56:55.798Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 103,
+      "amount": 3.45,
+      "recipient": "Paperbox",
+      "category": 6,
+      "date": "2016-06-06T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-06-06T17:05:10.178Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 97,
+      "amount": 4.99,
+      "recipient": "SportExports Filiale 1210",
+      "category": 2,
+      "date": "2016-06-04T00:00:00.000Z",
+      "is_need": false,
+      "child_id": 1,
+      "timestamp": "2016-06-04T13:15:17.813Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 5,
+      "amount": 1.2,
+      "recipient": "Fisher Spielwaren",
+      "category": 1,
+      "date": "2016-05-17T00:00:00.000Z",
+      "is_need": false,
+      "child_id": 1,
+      "timestamp": "2016-05-17T16:39:33.981Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 100,
+      "amount": 10,
+      "recipient": "A1",
+      "category": 5,
+      "date": "2016-06-15T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-06-15T17:00:33.392Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 99,
+      "amount": 7.5,
+      "recipient": "Billa",
+      "category": 0,
+      "date": "2016-06-06T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-06-06T15:30:34.177Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 9,
+      "amount": 1.2,
+      "recipient": "Mensa",
+      "category": 0,
+      "date": "2016-05-03T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-05-03T17:01:37.466Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 4,
+      "amount": 5.99,
+      "recipient": "H&M",
+      "category": 4,
+      "date": "2016-05-01T00:00:00.000Z",
+      "is_need": false,
+      "child_id": 1,
+      "timestamp": "2016-05-01T16:39:08.247Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 102,
+      "amount": 4.99,
+      "recipient": "H&M",
+      "category": 4,
+      "date": "2016-06-07T00:00:00.000Z",
+      "is_need": false,
+      "child_id": 1,
+      "timestamp": "2016-06-07T17:02:55.407Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 96,
+      "amount": 2.65,
+      "recipient": "Libro",
+      "category": 3,
+      "date": "2016-06-03T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-06-03T12:25:34.896Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 104,
+      "amount": 5,
+      "recipient": "Fisher Spielwaren",
+      "category": 1,
+      "date": "2016-06-06T00:00:00.000Z",
+      "is_need": false,
+      "child_id": 1,
+      "timestamp": "2016-06-06T10:18:38.247Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 98,
+      "amount": 2.65,
+      "recipient": "McDonalds",
+      "category": 0,
+      "date": "2016-06-05T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-06-05T14:41:10.862Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 2,
+      "amount": 2.5,
+      "recipient": "Billa",
+      "category": 0,
+      "date": "2016-05-05T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-05-05T12:38:54.624Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 6,
+      "amount": 1.77,
+      "recipient": "Der Mann",
+      "category": 0,
+      "date": "2016-05-14T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-05-14T16:41:38.640Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 10,
+      "amount": 7.33,
+      "recipient": "Billa",
+      "category": 0,
+      "date": "2016-05-25T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-05-25T17:03:40.882Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 11,
+      "amount": 4.56,
+      "recipient": "Libro",
+      "category": 3,
+      "date": "2016-05-12T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-05-12T17:04:04.099Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 12,
+      "amount": 13.12,
+      "recipient": "Hervis Sports",
+      "category": 2,
+      "date": "2016-05-09T00:00:00.000Z",
+      "is_need": false,
+      "child_id": 1,
+      "timestamp": "2016-05-09T17:04:17.824Z",
+      "type": 0,
+      "request_id": null
+    }, {
+      "id": 1,
+      "amount": 6.65,
+      "recipient": "Spar",
+      "category": 0,
+      "date": "2016-04-30T00:00:00.000Z",
+      "is_need": true,
+      "child_id": 1,
+      "timestamp": "2016-04-30T12:31:54.624Z",
+      "type": 0,
+      "request_id": null
+    }];*/
 
     return {
       all: function () {
@@ -190,19 +380,22 @@ angular.module('starter.services', [])
       setTransactions: function () {
         transactions = transactionsService.transactions();
       },
-      getTransactions: function() {
+      getTransactions: function () {
         return transactions;
       },
-      getLegend: function () {
+      getLegend: function (month, year) {
         var legend = [];
         for (var i = 0; i < transactions.length; i++) {
-          if (legend[transactions.category] == null) {
-            var category = {};
-            category.name = categories[transactions[i].category].name;
-            category.icon_android = categories[transactions[i].category].icon_android;
-            category.icon_ios = categories[transactions[i].category].icon_ios;
-            category.color = categories[transactions[i].category].color;
-            legend.push(category);
+          if (legend[transactions[i].category] == null) {
+            var date = new Date(transactions[i].date);
+            if (date.getMonth() == month && date.getFullYear() == year) {
+              var category = {};
+              category.name = categories[transactions[i].category].name;
+              category.icon_android = categories[transactions[i].category].icon_android;
+              category.icon_ios = categories[transactions[i].category].icon_ios;
+              category.color = categories[transactions[i].category].color;
+              legend[transactions[i].category] = category;
+            }
           }
         }
         return legend;
@@ -214,10 +407,13 @@ angular.module('starter.services', [])
         }
         return names;
       },
-      getSpent: function () {
+      getSpent: function (month, year) {
         var spent = [0, 0, 0, 0, 0, 0, 0];
         for (var i = 0; i < transactions.length; i++) {
-          spent[transactions[i].category] += transactions[i].amount;
+          var date = new Date(transactions[i].date);
+          if (date.getMonth() == month && date.getFullYear() == year) {
+            spent[transactions[i].category] += transactions[i].amount;
+          }
         }
         return spent;
       },
@@ -228,13 +424,14 @@ angular.module('starter.services', [])
         }
         return colors;
       },
-      getNeedWant: function () {
+      getNeedWant: function (month, year) {
         var needwant = [0, 0];
         for (var i = 0; i < transactions.length; i++) {
-          console.log(transactions[i].category);
-          categories[transactions[i].category].spent += transactions[i].amount;
-          if (transactions[i].is_need) needwant[0] += transactions[i].amount;
-          else needwant[1] += transactions[i].amount;
+          var date = new Date(transactions[i].date);
+          if (date.getMonth() == month && date.getFullYear() == year) {
+            if (transactions[i].is_need) needwant[0] += transactions[i].amount;
+            else needwant[1] += transactions[i].amount;
+          }
         }
         return needwant;
       },
